36e7fb01e7e92db8908b1028bc862ee984bff179
Ramzi Benchadi <mr.adem.dev@gmail.com>
2026-06-24 12:21:43 +0300

    fix: remove basepaths for custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: '/paxonflow', // <-- Double check this!
   trailingSlash: true,
   images: {
     unoptimized: true,
